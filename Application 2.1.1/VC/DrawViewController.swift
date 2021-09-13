68369d69a6e6e1426c5dea07ae3bd9845100e194
ltapp4 <laurentapp4@gmail.com>
2021-09-13 08:31:39 -0400

    Background color change

---
+++ Application 2.1.1/VC/DrawViewController.swift
@@ -0,0 +1,81 @@
+//
+//  DrawViewController.swift
+//  Application 2.1.1
+//
+//  Created by Lauren Tapp on 9/13/21.
+//
+
+import UIKit
+
+class Canvas : UIView {
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {return}
+        
+        context.setStrokeColor(UIColor.blue.cgColor)
+        context.setLineWidth(5)
+        context.setLineCap(.butt)
+        
+        lines.forEach { (line) in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        
+        context.strokePath()
+        
+    }
+    
+    
+    var lines = [[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else {return}
+        
+        guard  var lastLine = lines.popLast() else {
+            return
+        }
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+        setNeedsDisplay()
+        
+    }
+}
+
+class Drawing_ViewController: UIViewController {
+
+    let canvas = Canvas()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        //anvas.frame = view.frame
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
